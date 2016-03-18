33a52d8d82cb158499a0517ca0c2a8a467b32084
Enrique S <vampa@ciencias.unam.mx>
2016-03-17 20:14:32 -0600

    r que david lo pidio

--- Proyecto/app/controllers/posts_controller.rb
+++ Proyecto/app/controllers/posts_controller.rb
@@ -25,6 +25,9 @@ class PostsController < ApplicationController
   # POST /posts.json
   def create
     @post = Post.new(post_params)
+  
+    puts "USUARIO"
+    puts current_usuario.to_json
 
     respond_to do |format|
       if @post.save
@@ -69,6 +72,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:sangre, :comentario, :solved, :usuario_id, :usuarios_id)
+      params.require(:post).permit(:sangre, :comentario, :solved)
     end
 end
